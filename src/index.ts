ee415ecaf564aeb5926f7df4d7ecd7ba29280ca2
Jin1c on imini <Jin0x31c@gmail.com>
2024-11-09 15:36:41 +0800

    增加一个选项配置

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,37 @@ export const name = "card-guard";
 
 export interface Config {
   chance: number;
+  no_title: boolean;
   regex_str: string;
   prompt: string;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  chance: Schema.number().role("slider").min(0).max(1).step(0.05).default(0.35),
-  regex_str: Schema.string().required(),
+  chance: Schema.number()
+    .role("slider")
+    .min(0)
+    .max(1)
+    .step(0.05)
+    .default(0.35)
+    .description("检查群名片的概率"),
+  no_title: Schema.boolean()
+    .default(true)
+    .description("是否不检查带头衔的群友"),
+  regex_str: Schema.string()
+    .required()
+    .description("群名片的正则表达式，错误的表达式会导致插件不断报错"),
   prompt: Schema.string()
     .role("textarea")
     .default(
       "不合规范，请更改为：\n【本科入学年份-本科学校-保研学校（可以不填）-昵称】"
-    ),
+    )
+    .description("不合规范时的提示"),
 });
 
-export function apply(ctx: Context, { chance, regex_str, prompt }: Config) {
+export function apply(
+  ctx: Context,
+  { chance, no_title, regex_str, prompt }: Config
+) {
   const logger = ctx.logger("card-guard");
   ctx = ctx.guild();
   ctx.on("message", async (session) => {
@@ -39,7 +55,7 @@ export function apply(ctx: Context, { chance, regex_str, prompt }: Config) {
     const title = member_info.title;
     const card = member_info.card;
     const role = member_info.role;
-    if (title || role !== "member") return;
+    if ((title && no_title) || role !== "member") return;
     if (regex.test(card)) return;
     return session.send([
       h("at", { id: session.userId }),
