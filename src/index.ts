6a43ebef5a71f5631613617c4adb624e168d4c89
Jin1c on imini <Jin0x31c@gmail.com>
2024-11-29 15:23:08 +0800

    声明可重用

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import { Context, Schema, h } from "koishi";
+import { Context, Dict, Schema, h } from "koishi";
 import {} from "koishi-plugin-adapter-onebot";
 
 export const name = "card-guard";
+
+export const reusable = true; // 声明此插件可重用
+
+export const usage = `插件支持多份配置，请使用koishi原生的**过滤器**进行操作。
+
+在 OneBot 平台下， \`频道ID\` 就是 \`群号\` ，通过设置不同群号的过滤器，可以实现在不同群中使用不同正则约束的效果
+
+⬇️ 这就是过滤器`;
 
 export interface Config {
   chance: number;
   no_title: boolean;
   regex_str: string;
-  delete_message: DeleteMessage;
+  delete_message: DeleteMessage | Dict;
 }
 
 interface DeleteMessage {
@@ -29,16 +37,26 @@ export const Config: Schema<Config> = Schema.object({
   regex_str: Schema.string()
     .required()
     .description("群名片的正则表达式，错误的表达式会导致插件不断报错"),
-  delete_message: Schema.object({
-    enable: Schema.boolean().default(true).description("是否撤回提示消息"),
-    delay: Schema.number()
-      .role("slider")
-      .min(5)
-      .max(120)
-      .step(1)
-      .default(60)
-      .description("多少秒后撤回提示消息"),
-  }).description("撤回提示信息设置"),
+  delete_message: Schema.intersect([
+    Schema.object({
+      enable: Schema.boolean()
+        .default(false)
+        .description("是否撤回bot发出的提示消息"),
+    }).description("撤回提示信息设置"),
+    Schema.union([
+      Schema.object({
+        enable: Schema.const(true).required(),
+        delay: Schema.number()
+          .role("slider")
+          .min(5)
+          .max(120)
+          .step(1)
+          .default(60)
+          .description("多少秒后撤回提示消息"),
+      }),
+      Schema.object({}),
+    ]),
+  ]),
 });
 
 export function apply(
@@ -77,7 +95,7 @@ export function apply(
       ])
     );
     if (delete_message.enable) {
-      setTimeout(async () => {
+      ctx.setTimeout(async () => {
         try {
           for (let am of alert_message)
             await session.bot.deleteMessage(session.channelId, am);
